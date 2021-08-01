ed1331be7591f1c2e7822f0730604e42ef284db9
psyoblade <park.suhyuk@gmail.com>
2021-08-01 21:33:11 +0900

    [수정] 노트북 변환기

--- day5/convert_to_html.sh
+++ day5/convert_to_html.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm notebooks/*.html
-docker exec notebook jupyter nbconvert work/data-engineer-training-course-answer.ipynb --to html
-docker exec notebook jupyter nbconvert work/data-engineer-training-course.ipynb --to html
+docker-compose exec notebook sh -c "jupyter nbconvert work/*.ipynb --to html"
 mv notebooks/*.html notebooks/html
