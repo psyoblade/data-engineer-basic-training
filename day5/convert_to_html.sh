daa8859bb23dc78c5b1d9e73e769c974d2345728
psyoblade <park.suhyuk@gmail.com>
2021-08-01 21:26:11 +0900

    [수정] nbconvert 스크립트 수정

--- day5/convert_to_html.sh
+++ day5/convert_to_html.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm notebooks/*.html
-docker exec notebook jupyter nbconvert --to html work/data-engineer-training-course\*.ipynb
-mv notebooks/data-engineer-training-course*.html notebooks/html
+docker exec notebook jupyter nbconvert work/data-engineer-training-course-answer.ipynb --to html
+docker exec notebook jupyter nbconvert work/data-engineer-training-course.ipynb --to html
+mv notebooks/*.html notebooks/html
